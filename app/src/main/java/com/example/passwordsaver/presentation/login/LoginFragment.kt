e2d2d9cf6f541e601754e389cb1f1f4c66042d2c
unknown <mabopoi@gmail.com>
2021-10-26 22:14:14 -0300

    Add BiometricPrompt in LoginFragment

--- app/src/main/java/com/example/passwordsaver/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/passwordsaver/presentation/login/LoginFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -34,5 +36,46 @@ class LoginFragment : Fragment() {
                 }
             }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showBiometricPrompt()
+    }
+
+    private fun showBiometricPrompt() {
+        val biometricsIgnoredErrors = listOf(
+            BiometricPrompt.ERROR_NEGATIVE_BUTTON,
+            BiometricPrompt.ERROR_CANCELED,
+            BiometricPrompt.ERROR_USER_CANCELED,
+            BiometricPrompt.ERROR_NO_BIOMETRICS
+        )
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Auth required")
+            .setSubtitle("Prove you are the owner of this device!")
+            .setNegativeButtonText(getString(android.R.string.cancel))
+            .build()
+
+        val biometricPrompt = BiometricPrompt(
+            this,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    if (errorCode !in biometricsIgnoredErrors) {
+                        Toast.makeText(this@LoginFragment.context, errString, Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    Toast.makeText(this@LoginFragment.context, "Auth succeeded", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    Toast.makeText(this@LoginFragment.context, "Auth failed", Toast.LENGTH_LONG).show()
+                }
+            }
+        )
+
+        biometricPrompt.authenticate(promptInfo)
     }
 }
